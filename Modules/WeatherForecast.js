68cd8153e20b53a6ce121bef98a9f8ff1b2af06e
Tavitu97 <43606487+Tavitu97@users.noreply.github.com>
2021-10-25 11:07:40 +0300

    Wind Calculations

--- Modules/WeatherForecast.js
+++ Modules/WeatherForecast.js
@@ -39,29 +39,17 @@ module.exports = {
                 {
                     
                     let weather = JSON.parse(body);
-
-                    
-                    
-                    //  output it in the console just to make sure that the data is there
-                   // console.log(weather.daily[0].temp['min']);
+  
          
-                    if (weather.daily == undefined) 
+                    if (weather == undefined) 
                     {
                         //console.log("ana");
                         res.render('WeatherForecast', { weather: null, error: 'Error, please try again' });
                     } 
                     else
                     {
-                        //console.log(weather.daily[0]);
-                        for(i = 0; i < 7; i++)
-                        {
-                            //console.log(weather.daily);
-                           // lowTemperatures.push([AddDay(i),weather.daily[0].temp['min']]);
-                        }
-                     
-                       res.render('WeatherForecast',{ lowTemperatures: storeTemperatures("min",weather),highTemperatures: storeTemperatures("max",weather)});
-       
-        
+                       
+                       res.render('WeatherForecast',{ lowTemperatures: storeTemperatures("min",weather), highTemperatures: storeTemperatures("max",weather), windDirections:calculateWindDirection(weather),windSpeeds: retainWindSpeeds(weather)});
                     }
                
                 }
@@ -75,6 +63,116 @@ module.exports = {
 
 //check Min temperature for the next 7 days
 
+function retainWindSpeeds(forecast)
+{
+   var windSpeeds = [];
+
+   for(i = 0; i < 7; i++)
+   {
+       windSpeeds.push([AddDay(i),forecast.daily[i].wind_speed]);
+   }
+
+return windSpeeds;
+
+
+}
+
+
+function calculateWindDirection(forecast)
+{
+    var windDirections = [];
+
+    for(i = 0; i < 7; i++)
+    {
+        console.log();
+        var windDegrees = forecast.daily[i].wind_deg;
+
+            var j = (windDegrees + 11.25) % 360;
+            console.log(j);
+            
+                if (j <=  22.5)
+                {
+                  windDirections.push([AddDay(i), 'N']);
+                }
+                else if (j <=  45  ) 
+                {
+                    windDirections.push([AddDay(i), "NNE"]);
+                }
+                else if (j <=  67.5) 
+                {
+                    windDirections.push([AddDay(i), "NE"]);
+                }
+                else if (j <=  90  ) 
+                {
+                    windDirections.push([AddDay(i), "ENE"]);
+                }
+                else if (j <= 112.5) 
+                {
+                    windDirections.push([AddDay(i), "E"]);
+                    
+                }
+                else if (j <= 135  )
+                {
+                    windDirections.push([AddDay(i), "ESE"]);
+                }
+                else if (j <= 157.5) 
+                {
+                    windDirections.push([AddDay(i), "SE"]);
+                }
+                else if (j <= 180  ) 
+                {
+                    windDirections.push([AddDay(i), "SSE"]);
+                }
+
+                else if (j <= 202.5) 
+                {
+                    windDirections.push([AddDay(i), "S"]);
+                }
+
+                else if (j <= 225  )
+                {
+                    windDirections.push([AddDay(i), "SSW"]);
+                } 
+
+                else if (j <= 247.5) 
+                {
+                    windDirections.push([AddDay(i), "SW"]);
+                }
+
+                else if (j <= 270  ) 
+                {
+                    windDirections.push([AddDay(i), "WSW"]);
+                }
+
+                else if (j <= 292.5) 
+                {
+                    windDirections.push([AddDay(i), "W"]);
+                }
+
+                else if (j <= 315  )
+                {
+                    windDirections.push([AddDay(i), "WNW"]);
+                } 
+
+                else if (j <= 337.5) 
+                {
+                    windDirections.push([AddDay(i), "NW"]);
+                }
+
+                else
+                {
+                    windDirections.push([AddDay(i), "NNW"]);
+                }                 
+ 
+
+     }
+
+
+  return windDirections;
+
+
+}
+
 function storeTemperatures(info,forecast)
 {
     var temperatures = [];
@@ -83,14 +181,14 @@ function storeTemperatures(info,forecast)
         case "min":
             for(i = 0; i < 7; i++)
             {
-                    temperatures.push( [AddDay(i),forecast.daily[0].temp['min']] );
+                    temperatures.push( [AddDay(i),forecast.daily[i].temp['min']] );
             } 
             break;
 
         case "max":
             for(i = 0; i < 7; i++)
             {
-                    temperatures.push( [AddDay(i),forecast.daily[0].temp['max']] );
+                    temperatures.push( [AddDay(i),forecast.daily[i].temp['max']] );
             } 
 
     }
